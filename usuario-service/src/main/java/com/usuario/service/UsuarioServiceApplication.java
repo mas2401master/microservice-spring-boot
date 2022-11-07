4373f79f12a893050e73b1d37591ef603e0c9584
Miguel Angel Silva <94727218+mas2401master@users.noreply.github.com>
2022-11-07 08:19:31 -0400

    Eureka

--- usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
@@ -3,11 +3,14 @@ package com.usuario.service;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //@EnableFeignClients: para decirle que desde aqui se va a llamar otros microservicios
 @EnableFeignClients
 @SpringBootApplication
+//habilite este microservicio en el servidor de eureka
+@EnableEurekaClient 
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
